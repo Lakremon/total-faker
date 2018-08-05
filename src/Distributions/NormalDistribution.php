f647d4f58a4a34d0586c8f3d5c8613c72833945a
Lakremon <teapot_89@mail.ru>
2018-08-05 14:04:58 +0700

    new structure

---
+++ src/Distributions/NormalDistribution.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace TotalFaker\Distributions;
+
+
+class NormalDistribution
+{
+    public function getOne(iterable $pieces, int $expectedValue, float $standardDeviation): string
+    {
+
+    }
+
+    public function getRandomNumber(int $expectedValue, float $standardDeviation): float
+    {
+
+        return 0.1;
+    }
+}
---
+++ src/Helpers/Randomizer.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lakremon
+ * Date: 8/5/18
+ * Time: 2:01 PM
+ */
+
+namespace TotalFaker\Helpers;
+
+
+class Randomizer
+{
+    public static function getRandomInt(){
+        try{
+
+        }catch (\Exception $e){
+            
+        }
+    }
+}
