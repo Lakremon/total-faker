a6b18e804966d03940c9564a6e30e50d449ae90a
Lakremon <teapot_89@mail.ru>
2018-08-06 13:55:33 +0700

    fixes for Normal Distribution

--- src/Distributions/NormalDistribution.php
+++ src/Distributions/NormalDistribution.php
@@ -14,7 +14,7 @@ use TotalFaker\Helpers\Randomizer;
  * If you call this method 10000 times and insert values in one array. This array should use rounded value as a key
  * and count of got values as array value. Below you can see example of usage this function:
  *
- * array dump:
+ * array dump example:
  * <==========>
  * -4 => 2
  * -3 => 73
@@ -46,13 +46,23 @@ use TotalFaker\Helpers\Randomizer;
 class NormalDistribution
 {
     private $secondValue = null;
+    private static $normalDistributions = [];
     private $expectedValue;
     private $standardDeviation;
+    private $useShortTransformation;
 
-    public function __construct(float $expectedValue = 0, float $standardDeviation = 1)
+    /**
+     * NormalDistribution constructor.
+     * @param float $expectedValue
+     * @param float $standardDeviation
+     * @param bool $useShortTransformation - true if you need use short transformation without sin/cos usage. If you
+     * need full transformation make this param false.
+     */
+    public function __construct(float $expectedValue = 0, float $standardDeviation = 1, bool $useShortTransformation = true)
     {
         $this->expectedValue = $expectedValue;
         $this->standardDeviation = $standardDeviation;
+        $this->useShortTransformation = $useShortTransformation;
     }
 
     /**
@@ -60,10 +70,11 @@ class NormalDistribution
      * value.
      *
      * @param iterable $pieces
+     * @param bool $useShortTransformation
      * @return mixed
      * @throws \TotalFaker\Exceptions\RandomFunctionException
      */
-    public static function getOne(iterable $pieces)
+    public static function getOne(iterable $pieces, bool $useShortTransformation = true)
     {
         $normalizedArray = [];
         foreach ($pieces as $value) {
@@ -71,13 +82,19 @@ class NormalDistribution
         }
 
         $piecesCount = count($normalizedArray);
-        // Three standard deviations account for 99.7% probability
-        $standardDeviation = $piecesCount / 6;
-        $expectedValue = ($piecesCount - 1) / 2;
 
-        $normalDistribution = new NormalDistribution($expectedValue, $standardDeviation);
+        if (!key_exists($piecesCount, self::$normalDistributions)) {
+            // Three standard deviations account for 99.7% probability
+            $standardDeviation = $piecesCount / 6;
+            $expectedValue = ($piecesCount - 1) / 2;
+            self::$normalDistributions[$piecesCount] = new NormalDistribution(
+                $expectedValue,
+                $standardDeviation,
+                $useShortTransformation
+            );
+        }
 
-        $probablyKey = $normalDistribution->getRandomNumber();
+        $probablyKey = self::$normalDistributions[$piecesCount]->getRandomNumber();
 
         $normalizedKey = round($probablyKey);
         $normalizedKey = 0 > $normalizedKey ? 0 : $normalizedKey;
@@ -99,16 +116,45 @@ class NormalDistribution
             $this->secondValue = null;
             return $value;
         } else {
-            do {
-                $randomValue1 = Randomizer::getRandomZeroOne() * 2 - 1;
-                $randomValue2 = Randomizer::getRandomZeroOne() * 2 - 1;
-                $circleSquare = pow($randomValue1, 2) + pow($randomValue2, 2);
-            } while ($circleSquare > 1.0 || $circleSquare == 0.0);
-            $circleRadius = sqrt(-2 * log($circleSquare) / $circleSquare);
-
-            $this->secondValue = $this->expectedValue + $circleRadius * $randomValue2 * $this->standardDeviation;
-
-            return $this->expectedValue + $circleRadius * $randomValue1 * $this->standardDeviation;
+            if ($this->useShortTransformation) {
+                return $this->shortTransformation();
+            } else {
+                return $this->fullTransformation();
+            }
         }
+    }
+
+    /**
+     * @return float
+     * @throws \TotalFaker\Exceptions\RandomFunctionException
+     */
+    private function shortTransformation(): float
+    {
+        do {
+            $randomValue1 = Randomizer::getRandomZeroOne() * 2 - 1;
+            $randomValue2 = Randomizer::getRandomZeroOne() * 2 - 1;
+            $circleSquare = pow($randomValue1, 2) + pow($randomValue2, 2);
+        } while ($circleSquare > 1.0 || $circleSquare == 0.0);
+        $circleRadius = sqrt(-2 * log($circleSquare) / $circleSquare);
+
+        $this->secondValue = $this->expectedValue + $circleRadius * $randomValue2 * $this->standardDeviation;
+
+        return $this->expectedValue + $circleRadius * $randomValue1 * $this->standardDeviation;
+    }
+
+    /**
+     * @return float
+     * @throws \TotalFaker\Exceptions\RandomFunctionException
+     */
+    private function fullTransformation(): float
+    {
+        $randomValue1 = Randomizer::getRandomZeroOne();
+        $randomValue2 = Randomizer::getRandomZeroOne();
+        $normalRandomValue1 = sqrt(-2 * log($randomValue1)) * cos(2 * M_PI * $randomValue2);
+        $normalRandomValue2 = sqrt(-2 * log($randomValue1)) * sin(2 * M_PI * $randomValue2);
+
+        $this->secondValue = $this->expectedValue + $normalRandomValue2 * $this->standardDeviation;
+
+        return $this->expectedValue + $normalRandomValue1 * $this->standardDeviation;
     }
 }
--- tests/NormalDistributionTest.php
+++ tests/NormalDistributionTest.php
@@ -29,12 +29,47 @@ class NormalDistributionTest extends TestCase
         echo "\r\n";
     }
 
+    public function testRandomNumberFunctionFull(): void
+    {
+        $normalDistribution = new NormalDistribution(0, 1, false);
+        $values = [];
+        for ($i = 0; $i < 10000; $i++) {
+            $number = $normalDistribution->getRandomNumber();
+            $key = intval(round($number));
+            if (!key_exists($key, $values)) {
+                $values[$key] = 0;
+            }
+            $values[$key]++;
+        }
+        ksort($values);
+
+        $this->assertEquals(10000, array_sum($values));
+        foreach ($values as $key => $value) {
+            echo "{$key} => {$value}\r\n";
+        }
+        echo "\r\n";
+    }
+
     public function testGetOneFunction()
     {
         $valuesPull = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
         $values = array_fill_keys($valuesPull, 0);
         for ($i = 0; $i < 10000; $i++) {
             $values[NormalDistribution::getOne($valuesPull)]++;
+        }
+        $this->assertEquals(10000, array_sum($values));
+        foreach ($values as $key => $value) {
+            echo "{$key} => {$value}\r\n";
+        }
+        echo "\r\n";
+    }
+
+    public function testGetOneFunctionFull()
+    {
+        $valuesPull = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+        $values = array_fill_keys($valuesPull, 0);
+        for ($i = 0; $i < 10000; $i++) {
+            $values[NormalDistribution::getOne($valuesPull, false)]++;
         }
         $this->assertEquals(10000, array_sum($values));
         foreach ($values as $key => $value) {
