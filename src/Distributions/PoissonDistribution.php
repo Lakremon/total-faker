5496d32da5e504d1f7147cf318bbceeb3418be73
Lakremon <teapot_89@mail.ru>
2018-08-12 22:33:21 +0700

    Poisson distribution fix + description.

--- src/Distributions/PoissonDistribution.php
+++ src/Distributions/PoissonDistribution.php
@@ -2,40 +2,80 @@
 
 namespace TotalFaker\Distributions;
 
-
-use TotalFaker\Helpers\Randomizer;
-use TotalFaker\Helpers\Math;
-
+/**
+ * Class PoissonDistribution to get Poisson distribution emulation. This class transform exponential distribution to
+ * Poisson distribution. It use Poisson distribution feature. Main method is getRandomNumber(). It returns only integer
+ * numbers. More information here - https://en.wikipedia.org/wiki/Poisson_distribution
+ *
+ * If you call this method 10000 times and insert values in one array. This array should use rounded value as a key
+ * and count of got values as array value. Below you can see example of usage this function:
+ *
+ * array dump example:
+ * <==========>
+ * 0 => 3690
+ * 1 => 3729
+ * 2 => 1797
+ * 3 => 554
+ * 4 => 175
+ * 5 => 50
+ * 6 => 4
+ * 7 => 1
+ * <==========>
+ *
+ * times  ^
+ *        |
+ *        *  *
+ *        |
+ *        |
+ *        |
+ *        |     *
+ *        |
+ *        |        *
+ *        |              *
+ *        +--|--|--|--|--|--|------> values
+ *
+ *
+ * @package TotalFaker\Distributions
+ */
 class PoissonDistribution
 {
-    private $expectedValue;
+    private $rate;
+    private static $exponentialDistribution;
 
-    public function __construct(float $expectedValue = 1)
+    /**
+     * PoissonDistribution constructor.
+     * @param int $rate
+     */
+    public function __construct(int $rate = 1)
     {
-        $this->expectedValue = $expectedValue;
+        $this->rate = $rate;
     }
 
     /**
-     * Get one number in Poisson Distribution
-     *
-     * @return float
+     * Get random number via Poisson distribution.
+     * @return int
      * @throws \TotalFaker\Exceptions\RandomFunctionException
      */
     public function getRandomNumber(): int
     {
-//        $count = 0;
-//
-//        do {
-//            $count++;
-//            $randomValue1 = Randomizer::getRandomInt(0, $this->expectedValue);
-//        } while ($randomValue1 !== 0);
-        $count = 0;
-        $randomValue1 = Randomizer::getRandomInt(0);
-        for ($i = 0; $i <= $randomValue1; $i++) {
-            $count += pow($this->expectedValue, $i) / Math::fact($i);
+        $s = 0;
+
+        for ($i = -1; $s < $this->rate; $i++) {
+            $s += self::getRandomExponential();
         }
-        return $count * pow(M_E, -$this->expectedValue);
+        return $i;
     }
 
-
+    /**
+     * Get random number via exponential distribution with rate one.
+     * @return float
+     * @throws \TotalFaker\Exceptions\RandomFunctionException
+     */
+    private static function getRandomExponential(): float
+    {
+        if (empty(self::$exponentialDistribution)) {
+            self::$exponentialDistribution = new ExponentialDistribution();
+        }
+        return self::$exponentialDistribution->getRandomNumber();
+    }
 }
